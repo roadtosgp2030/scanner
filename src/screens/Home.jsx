27a608f209737ade451558eaf17fea03b35843b5
Vu Duc <ducvn@vaixgroup.com>
2024-08-21 09:23:06 +0700

    add navigate

--- src/screens/Home.jsx
+++ src/screens/Home.jsx
@@ -1,10 +1,15 @@
 import { Scanner, useDevices } from '@yudiel/react-qr-scanner'
+import { useNavigate } from 'react-router-dom';
 
 const Home = () => {
     const devices = useDevices()
+    const navigate = useNavigate()
+
     console.log(devices);
 
-    return <Scanner onScan={result => console.log(result)} />;
+    return <Scanner onScan={() => {
+        navigate('/test')
+    }} />;
 };
 
 export default Home;
